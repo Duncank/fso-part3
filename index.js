548535346058a607cb72ddc0867679667bcb79e7
Duncan Krebbers <duncan@projectfive.nl>
2019-10-26 15:27:07 +0200

    3.4 - delete

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
-const phonebook = [
+let phonebook = [
     {
         name: 'Arto Hellas',
         number: '1234567890',
@@ -35,6 +35,13 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end();
     }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    phonebook = phonebook.filter(it => it.id !== id);
+
+    res.status(204).end();
 });
 
 app.get('/info', (req, res) => {
